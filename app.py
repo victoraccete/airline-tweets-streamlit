fce638f07fee1c5a7bc5ff9039f2228f016c286b
Victor Accete <victor.accete@gmail.com>
2020-10-05 00:23:32 -0300

    attempting to plot map

--- app.py
+++ app.py
@@ -13,15 +13,24 @@ DESCRIPTION = "This application is a streamlit dashboard to make easier the visu
 st.markdown(DESCRIPTION)
 st.sidebar.markdown(DESCRIPTION)
 
+def split_coordinates(df: pd.DataFrame) -> pd.DataFrame:
+    """splits the values from the tweet_coords column to two new columns called Latitude and Longitude"""
+    coords_df = df['tweet_coord'].str.strip('[]').str.split(', ', expand=True).rename(columns={0:'lat', 1:'lon'})
+    st.write(coords_df)
+    coords_df.lat = coords_df.lat.astype('float128', copy=False)
+    coords_df.lon = coords_df.lon.astype('float128', copy=False)
+    df = pd.concat([df, coords_df], axis=1, sort=False)
+    return df
+    
 DATA_PATH = "Tweets.csv"
-
 @st.cache(persist=True)
 def load_data() -> pd.DataFrame:
     data = pd.read_csv(DATA_PATH)
     data.tweet_created = pd.to_datetime(data.tweet_created)
+    data = split_coordinates(data)
     return data
-
 data = load_data()
+st.write(data)
 
 st.sidebar.subheader("Show random tweet.")
 random_tweet = st.sidebar.radio("Sentiment", ('positive', 'neutral', 'negative'))
@@ -43,6 +52,8 @@ if st.sidebar.checkbox("Hide", True) == False:
         fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
 
+st.write(data.dtypes)
+st.map(data)
 
 
 
