9164e3383bceae1ba821f41774c7f46617da2d62
Victor Accete <victor.accete@gmail.com>
2020-10-04 22:02:02 -0300

    adding dataset

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello, world!")
-st.markdown("My first streamlit dashboard!")
+title = "Sentiment analysis of tweets on US Airlines"
+st.title(title)
+st.sidebar.title(title)
+
+description = "This application is a streamlit dashboard to make easier the visualization of sentiment analysis of tweets 🐦"
+st.markdown(description)
+st.sidebar.markdown(description)
+"""
+DATA_PATH = "Tweets.csv"
+def load_data():
+    data = pd.read_csv(DATA_PATH)"""
