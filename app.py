3f6b6f7ce2f672dc26976b74cf5505eaaa74e419
Victor Accete <victor.accete@gmail.com>
2020-10-04 23:18:13 -0300

    adding pie or bar chart dropdown

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+# imports
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
 
+# app
 TITLE = "Sentiment analysis of tweets on US Airlines"
 st.title(TITLE)
 st.sidebar.title(TITLE)
@@ -25,3 +27,23 @@ st.sidebar.subheader("Show random tweet.")
 random_tweet = st.sidebar.radio("Sentiment", ('positive', 'neutral', 'negative'))
 # displaying the tweet as md text and using .sample(n=1) to get only one tweet:
 st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0, 0])
+
+st.sidebar.markdown("### Number of tweets by sentiment")
+select_visual = st.sidebar.selectbox('Visualization type', ['Bars', 'Pie'], key='1')
+sentiment_count = data['airline_sentiment'].value_counts()
+sentiment_count = pd.DataFrame({'Sentiment': sentiment_count.index, 
+                               'Tweets': sentiment_count.values})
+
+if st.sidebar.checkbox("Hide", True) == False:
+    st.markdown("### Number of tweets by sentiment")
+    if select_visual == 'Bars':
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+
+
+
+
