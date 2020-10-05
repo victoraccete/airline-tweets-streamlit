51608270d65278eabe8ea34bc05a024b3e7f8e43
Victor Accete <victor.accete@gmail.com>
2020-10-04 22:41:41 -0300

    updating readme and creating radio buttons on sidebar

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # airline-tweets-streamlit
-Getting started with streamlit for building interactive dashboards. Dataset here: https://www.kaggle.com/crowdflower/twitter-airline-sentiment
+Getting started with streamlit for building interactive dashboards. Dataset here: https://www.kaggle.com/crowdflower/twitter-airline-sentiment  
+
+**Need to have:**  
+Streamlit
+Pandas
+Numpy
+Plotly express
--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
-title = "Sentiment analysis of tweets on US Airlines"
-st.title(title)
-st.sidebar.title(title)
+TITLE = "Sentiment analysis of tweets on US Airlines"
+st.title(TITLE)
+st.sidebar.title(TITLE)
 
-description = "This application is a streamlit dashboard to make easier the visualization of sentiment analysis of tweets 🐦"
-st.markdown(description)
-st.sidebar.markdown(description)
-"""
+DESCRIPTION = "This application is a streamlit dashboard to make easier the visualization of sentiment analysis of tweets 🐦"
+st.markdown(DESCRIPTION)
+st.sidebar.markdown(DESCRIPTION)
+
 DATA_PATH = "Tweets.csv"
-def load_data():
-    data = pd.read_csv(DATA_PATH)"""
+
+@st.cache(persist=True)
+def load_data() -> pd.DataFrame:
+    data = pd.read_csv(DATA_PATH)
+    data.tweet_created = pd.to_datetime(data.tweet_created)
+    return data
+
+data = load_data()
+
+st.sidebar.subheader("Show random tweet.")
+random_tweet = st.sidebar.radio("Sentiment", ('positive', 'neutral', 'negative'))
+# displaying the tweet as md text and using .sample(n=1) to get only one tweet:
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0, 0])
